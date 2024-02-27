b2767deceec258c5f30f10df0d4f9d9bca049cbf
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-27 20:20:25 +0200

    Implement TestListener

--- src/test/java/lt/julija/util/TestListener.java
+++ src/test/java/lt/julija/util/TestListener.java
@@ -1,4 +1,35 @@
 package lt.julija.util;
 
-public class TestListener {
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestListener implements ITestListener {
+    @Override
+    public void onTestFailure(ITestResult result) {
+        takeScreenshot();
+    }
+
+    private void takeScreenshot() {
+        TakesScreenshot screenshot = (TakesScreenshot) Driver.getDriver();
+        File fileScreenshot = screenshot.getScreenshotAs(OutputType.FILE);
+
+        LocalDateTime currentDate = LocalDateTime.now();
+        DateTimeFormatter dateForm = DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss_SSS");
+
+        File newFile = new File("./screenshots/" + currentDate.format(dateForm) + "screenshot.png");
+
+        try {
+            FileUtils.copyFile(fileScreenshot, newFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
