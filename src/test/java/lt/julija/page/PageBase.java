06e4f05df67f843468da972e4d15537f7e11826e
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-27 20:10:32 +0200

    Add open method into PageBase class

--- src/test/java/lt/julija/page/PageBase.java
+++ src/test/java/lt/julija/page/PageBase.java
@@ -1,4 +1,8 @@
 package lt.julija.page;
 
 public class PageBase {
+    public static void open(String url, int seconds){
+        Common.setUpDriver(seconds);
+        Common.openUrl(url);
+    }
 }
