c5015eba1d438d80eba53d3c33e755ab802e5678
julijajuskeviciene <julija.juskeviciene@gmail.com>
2024-02-27 20:10:12 +0200

    Add tests with DriverManager

--- src/test/java/lt/julija/util/Driver.java
+++ src/test/java/lt/julija/util/Driver.java
@@ -1,4 +1,27 @@
 package lt.julija.util;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class Driver {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+
+        driver = new ChromeDriver(options);
+
+        driver.get("chrome://settings/");
+        ((JavascriptExecutor) driver).executeScript("chrome.settingsPrivate.setDefaultZoom(0.70)");
+    }
 }
